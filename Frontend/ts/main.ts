4923b39a07eef5b9202bfa380a7a94516b81b60e
Scott Techau <scottbot95@gmail.com>
2019-06-29 17:59:15 -0700

    Clean up script imports/exports

--- Frontend/ts/main.ts
+++ Frontend/ts/main.ts
@@ -1,11 +1,10 @@
 import 'bootstrap';
-import * as jquery from 'jquery';
+import * as jQuery from 'jquery';
 import 'jquery-validation';
 import 'jquery-validation-unobtrusive';
 
 import '../css/main.scss';
 
-(window as any).jQuery = jquery;
-
+export {jQuery};
 
 export * from './mods/uploader';
