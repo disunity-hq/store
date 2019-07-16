705678d02c4489e1060c1f78705f8f368a7f9114
Scott Techau <scottbot95@gmail.com>
2019-07-16 10:20:01 -0700

    Incoporate syncfusion styles into webpack to allow theming

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -7,62 +7,70 @@ const CopyPlugin = require('copy-webpack-plugin');
 const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
 
 const entries = (() => {
-    const pagesPath = path.join(__dirname, 'ts/pages');
-    console.log(`pagesPath: ${pagesPath}`);
-    var pages = glob.sync(`${pagesPath}/**/*.ts`);
-    return pages.reduce((acc, item) => {
-        var relPath = path.relative(pagesPath, item);
-        var keyName = relPath.replace(/\.[^/.]+$/, "");
-        acc[keyName] = item;
-        return acc;
-    }, {
-        'main': path.join(__dirname, 'ts/main.ts')
-    });
+  const pagesPath = path.join(__dirname, 'ts/pages');
+  console.log(`pagesPath: ${pagesPath}`);
+  var pages = glob.sync(`${pagesPath}/**/*.ts`);
+  return pages.reduce(
+    (acc, item) => {
+      var relPath = path.relative(pagesPath, item);
+      var keyName = relPath.replace(/\.[^/.]+$/, '');
+      acc[keyName] = item;
+      return acc;
+    },
+    {
+      main: path.join(__dirname, 'ts/main.ts')
+    }
+  );
 })();
 
 for (let key in entries) {
-    console.log(key);
+  console.log(key);
 }
 
 module.exports = {
-    entry: entries,
-    output: {
-        path: path.join(__dirname, 'dist'),
-        libraryTarget: 'window',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                loader: "html-loader?exportAsEs6Default"
-            },
-            {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(css|scss|sass)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: function () {
-                                return [require('autoprefixer')];
-                            }
-                        }
-                    },
-                    'sass-loader'
-                ]
+  entry: entries,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    libraryTarget: 'window'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'html-loader?exportAsEs6Default'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [require('autoprefixer')];
+              }
             }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['node_modules/@syncfusion']
+            }
+          }
         ]
-    },
-    resolve: {
-        plugins: [ new TsConfigPathsPlugin() ],
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.html']
-    },
-    plugins: [new CopyPlugin(['favicon.ico', {from:'assets', to:'assets'}])],
+      }
+    ]
+  },
+  resolve: {
+    plugins: [new TsConfigPathsPlugin()],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.html']
+  },
+  plugins: [new CopyPlugin(['favicon.ico', { from: 'assets', to: 'assets' }])]
 };
