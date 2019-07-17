be4325e2c45e3c051695341ef3d4d75a6a03a004
Scott Techau <scottbot95@gmail.com>
2019-07-16 22:23:38 -0700

    Improve filtering and add order by logic for target list

--- Disunity.Store/Pages/Targets/Index.cshtml.cs
+++ Disunity.Store/Pages/Targets/Index.cshtml.cs
@@ -1,25 +1,23 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mime;
-using System.Threading.Tasks;
-
 using Disunity.Store.Entities;
 using Disunity.Store.Shared.Data;
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-
 using SmartBreadcrumbs.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 
-namespace Disunity.Store.Pages.Targets {
+namespace Disunity.Store.Pages.Targets
+{
 
     [Breadcrumb("Targets")]
-    public class Index : PageModel {
+    public class Index : PageModel
+    {
 
         private readonly ApplicationDbContext _context;
         private readonly ILogger<Index> _logger;
@@ -27,19 +25,39 @@ namespace Disunity.Store.Pages.Targets {
         public IList<Target> Targets { get; set; }
 
         [BindProperty(SupportsGet = true)] public string Title { get; set; }
+        [BindProperty(SupportsGet = true)] public string OrderBy { get; set; } = "Name";
 
-        public Index(ApplicationDbContext context, ILogger<Index> logger) {
+        public Dictionary<string, Expression<Func<Target, IComparable>>> OrderOptions = new Dictionary<string, Expression<Func<Target, IComparable>>>() {
+            {"Name",  t=>t.DisplayName
+    },
+            {"Total Mods",  t=>t.CompatibleMods.Count()
+},
+        };
+
+        public Index(ApplicationDbContext context, ILogger<Index> logger)
+        {
             _context = context;
             _logger = logger;
         }
 
-        public async Task OnGetAsync() {
+        public async Task OnGetAsync()
+        {
             IQueryable<Target> targets = _context.Targets
                                                  .Include(t => t.Latest)
                                                  .Include(t => t.Compatibilities);
 
-            if (!string.IsNullOrWhiteSpace(Title)) {
+            if (!string.IsNullOrWhiteSpace(Title))
+            {
                 targets = targets.Where(t => EF.Functions.ILike(t.Latest.DisplayName, $"%{Title}%"));
+            }
+
+            if (!string.IsNullOrWhiteSpace(OrderBy))
+            {
+                var ordering = OrderOptions.GetValueOrDefault(OrderBy);
+                if (ordering != null)
+                {
+                    targets = targets.OrderBy(ordering);
+                }
             }
 
             Targets = await targets.ToListAsync();
