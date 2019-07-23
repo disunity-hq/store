f71bf1cd2cbc1d0e26d0ccc64d1e8f032cfa8311
Dustin Lacewell <dlacewell@gmail.com>
2019-07-23 09:06:13 -0500

    Make it so inline editor availability is based on policy

--- Disunity.Store/Pages/Users/Details.cshtml.cs
+++ Disunity.Store/Pages/Users/Details.cshtml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Disunity.Store.Data;
 using Disunity.Store.Entities;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -19,15 +20,18 @@ namespace Disunity.Store.Pages.Users {
 
         private readonly ILogger<Details> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly IAuthorizationService _authorization;
+
+        public bool CanManageOrg;
 
         [FromRoute] public string UserSlug { get; set; }
 
         public Org Org { get; set; }
 
-        public Details(ILogger<Details> logger, ApplicationDbContext context) {
+        public Details(ILogger<Details> logger, ApplicationDbContext context, IAuthorizationService authorization) {
             _logger = logger;
             _context = context;
-
+            _authorization = authorization;
         }
 
         public async Task<IActionResult> OnGetAsync() {
@@ -40,6 +44,9 @@ namespace Disunity.Store.Pages.Users {
             if (Org == null) {
                 return NotFound();
             }
+
+            var authorization = await _authorization.AuthorizeAsync(User, "CanManageOrg");
+            CanManageOrg = authorization.Succeeded;
 
             ViewData["BreadcrumbNode"] = new RazorPageBreadcrumbNode("/Users/Details", Org.DisplayName) {
                 RouteValues = new {UserSlug}
