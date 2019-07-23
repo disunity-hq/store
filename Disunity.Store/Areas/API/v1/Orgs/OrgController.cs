67b1e348e2b5b44a92f7d6a8da6a525cfe098745
Dustin Lacewell <dlacewell@gmail.com>
2019-07-22 23:09:12 -0500

    Add inline org displayname editor

---
+++ Disunity.Store/Areas/API/v1/Orgs/OrgController.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+using Disunity.Store.Data;
+using Disunity.Store.Entities;
+using Disunity.Store.Pages.Mods;
+using Disunity.Store.Artifacts;
+using Disunity.Store.Data;
+using Disunity.Store.Entities.DataTransferObjects;
+using Disunity.Store.Storage;
+using Disunity.Store.Storage.Backblaze;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json.Schema;
+
+
+namespace Disunity.Store.Areas.API.v1.Orgs {
+
+    [Bind("Name", "PrimaryKey", "Value")]
+    public class OrgInput {
+        public string name { get; set; }
+        public string primaryKey { get; set; }
+        public string value { get; set; }
+    }
+
+    [ApiController]
+    [Route("api/v{version:apiVersion}/orgs/{orgSlug:slug}")]
+    public class OrgController : ControllerBase {
+
+        private readonly ILogger<Upload> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<UserIdentity> _userManager;
+        private readonly IMapper _mapper;
+
+        public OrgController(ILogger<Upload> logger, ApplicationDbContext context,
+                             UserManager<UserIdentity> userManager, IMapper mapper) {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        [FromBody]
+        public OrgInput SubmitModel { get; set; }
+//
+//        protected object FormatSchemaError(ValidationError e) {
+//            return new {
+//                Kind = e.ErrorType,
+//                e.Path,
+//                e.Message,
+//                e.LineNumber,
+//                e.LinePosition
+//            };
+//        }
+
+        [HttpGet]
+        public IActionResult Get(string orgSlug) {
+            var org = _context.Orgs.SingleOrDefault(o => o.Slug == orgSlug);
+
+            if (org == null) {
+                return new NotFoundResult();
+            }
+
+            return new JsonResult($"Hello from {orgSlug}!");
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "IsMember")]
+        public async Task<IActionResult> PostAsync(string orgSlug) {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = reader.ReadToEnd();
+                _logger.LogWarning(body);
+            }
+
+            if (!ModelState.IsValid) {
+                return BadRequest();
+            }
+
+            try {
+                var org = _context.Orgs.SingleOrDefault(o => o.Slug == orgSlug);
+
+                if (org == null) {
+                    return new NotFoundResult();
+                }
+
+                org.DisplayName = SubmitModel.value;
+
+                await _context.SaveChangesAsync();
+
+                return new JsonResult(_mapper.Map<OrgDto>(org));
+            }
+            catch (Exception e) {
+                var Type = e.GetType().Name;
+                var Errors = new object[] { };
+
+//                if (e is ManifestSchemaException schemaExc) {
+//                    Errors = schemaExc.Errors.Select(FormatSchemaError).ToArray();
+//                } else if (e is ArchiveFormFileValidationError formFileError) {
+//                    Errors = new[] {formFileError.Message};
+//                } else if (e is ArchiveLoadException archiveExc) {
+//                    Errors = new[] {archiveExc.Message};
+//                } else {
+//                    _logger.LogError(e, "");
+//                }
+
+                return BadRequest(new {Type, Errors});
+            }
+        }
+
+    }
+
+}
