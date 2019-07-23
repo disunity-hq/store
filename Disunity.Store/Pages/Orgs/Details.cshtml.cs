37e03fecae9ba0ae8192c045e806c439768f50a2
Scott Techau <scottbot95@gmail.com>
2019-07-23 13:09:43 -0700

    Refactor user and org details to use shared partials

--- Disunity.Store/Pages/Orgs/Details.cshtml.cs
+++ Disunity.Store/Pages/Orgs/Details.cshtml.cs
@@ -1,12 +1,38 @@
+using System.Threading.Tasks;
+
+using Disunity.Store.Data;
+using Disunity.Store.Entities;
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+using SmartBreadcrumbs.Nodes;
 
 
 namespace Disunity.Store.Pages.Orgs {
 
     public class Details : PageModel {
 
-        public void OnGet() {
-            
+        private readonly ILogger<Details> _logger;
+        private readonly ApplicationDbContext _context;
+
+        [FromRoute] public string UserSlug { get; set; }
+
+        public Org Org { get; set; }
+
+        public async Task OnGetAsync() {
+            Org = await _context.Orgs
+                                .Include(o => o.Mods)
+                                .ThenInclude(m => m.Latest)
+                                .ThenInclude(v => v.VersionNumber)
+                                .FirstOrDefaultAsync(o => o.Slug == UserSlug);
+
+            ViewData["BreadcrumbNode"] = new RazorPageBreadcrumbNode("/Users/Details", Org.DisplayName) {
+                RouteValues = new {UserSlug}
+            };
+
         }
 
     }
--- Disunity.Store/Pages/Users/Details.cshtml.cs
+++ Disunity.Store/Pages/Users/Details.cshtml.cs
@@ -20,39 +20,27 @@ namespace Disunity.Store.Pages.Users {
 
         private readonly ILogger<Details> _logger;
         private readonly ApplicationDbContext _context;
-        private readonly IAuthorizationService _authorization;
-
-        public bool CanManageOrg;
 
         [FromRoute] public string UserSlug { get; set; }
 
         public Org Org { get; set; }
 
-        public Details(ILogger<Details> logger, ApplicationDbContext context, IAuthorizationService authorization) {
+        public Details(ILogger<Details> logger, ApplicationDbContext context) {
             _logger = logger;
             _context = context;
-            _authorization = authorization;
         }
 
-        public async Task<IActionResult> OnGetAsync() {
+        public async Task OnGetAsync() {
             Org = await _context.Orgs
                                 .Include(o => o.Mods)
                                 .ThenInclude(m => m.Latest)
                                 .ThenInclude(v => v.VersionNumber)
                                 .FirstOrDefaultAsync(o => o.Slug == UserSlug);
 
-            if (Org == null) {
-                return NotFound();
-            }
-
-            var authorization = await _authorization.AuthorizeAsync(User, "CanManageOrg");
-            CanManageOrg = authorization.Succeeded;
-
             ViewData["BreadcrumbNode"] = new RazorPageBreadcrumbNode("/Users/Details", Org.DisplayName) {
                 RouteValues = new {UserSlug}
             };
 
-            return Page();
         }
 
     }
