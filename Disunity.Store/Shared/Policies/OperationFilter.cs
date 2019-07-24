4822caac5ea2c8ad6749732f4eeed4a43aff8411
Dustin Lacewell <dlacewell@gmail.com>
2019-07-23 20:23:03 -0500

    Remove debug logging

--- Disunity.Store/Shared/Policies/OperationFilter.cs
+++ Disunity.Store/Shared/Policies/OperationFilter.cs
@@ -20,26 +20,18 @@ namespace Disunity.Store.Policies {
         public void OnAuthorization(AuthorizationFilterContext context) {
             var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<OperationFilter>>();
             var authService = context.HttpContext.RequestServices.GetRequiredService<IAuthorizationService>();
-            logger.LogWarning($"Descriptor: {context.ActionDescriptor.GetType().Name}");
             var desc = context.ActionDescriptor as ControllerActionDescriptor;
 
             if (desc == null) {
-                logger.LogWarning("descriptor was null");
                 return;
             }
 
             var attrs = desc.MethodInfo.GetCustomAttributes(typeof(OperationAttribute), true);
 
-            logger.LogWarning($"found {attrs.Length} attrs on {desc.MethodInfo.Name}");
-
             foreach (OperationAttribute attr in attrs) {
-                logger.LogWarning($"Found attr: {attr.GetType().Name}");
-                logger.LogWarning($"With op: {attr.Operation}");
-
                 var requirement = attr.GetRequirement();
 
                 if (requirement == null) {
-                    logger.LogWarning("Requirement was null.");
                     context.Result = new ForbidResult();
                     return;
                 }
@@ -47,14 +39,11 @@ namespace Disunity.Store.Policies {
                 var resource = attr.GetResource(context);
 
                 if (resource == null) {
-                    logger.LogWarning("Resource was null.");
                     context.Result = new ForbidResult();
                     return;
                 }
 
-
                 var authorization = authService.AuthorizeAsync(context.HttpContext.User, resource, requirement).Result;
-                logger.LogWarning($"The authz succeeded: {authorization.Succeeded}");
 
                 if (!authorization.Succeeded) {
                     context.Result = new ForbidResult();
