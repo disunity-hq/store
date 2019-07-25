e12bda5d4c3aa07b8b0fc5c5b66100f86cf84f19
Scott Techau <scottbot95@gmail.com>
2019-07-25 16:42:38 -0700

    Update package list

--- Frontend/ts/shared/vue/syncfusion.ts
+++ Frontend/ts/shared/vue/syncfusion.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
 import { InPlaceEditorPlugin } from '@syncfusion/ej2-vue-inplace-editor';
+import { AutoCompletePlugin } from '@syncfusion/ej2-vue-dropdowns';
 
 Vue.use(InPlaceEditorPlugin);
+Vue.use(AutoCompletePlugin);
